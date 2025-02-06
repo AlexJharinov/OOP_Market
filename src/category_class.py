85333e77dff857fe1eb1e83a65c61e81c7559ad3
Alex <neftkom.otk@gmail.com>
2025-02-07 02:16:51 +0400

    Создана структура проекта, добавлены модули utils, category_class, product_class в них реалезованы функции и добавлены класы согласно ТЗ, а так же добавлен файл json.

---
+++ src/category_class.py
@@ -0,0 +1,22 @@
+
+
+class Category:
+    """Класс для работы с категориями"""
+
+    category_count = 0
+    product_count = 0
+
+    name = str
+    description = str
+    products = list
+
+
+    def __init__(self, name, description, products):
+        """Функция инициализации категории"""
+
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+from unicodedata import category
+
+from src.category_class import Category
+from src.product_class import Product
+
+
+def read_json(path: str) -> dict:
+    full_path = os.path.abspath(path)
+    with open(full_path, 'r', encoding="UTF-8") as f:
+        data = json.load(f)
+    return data
+
+def creat_objects(data):
+    category_list = []
+    for cat in data:
+        prod = []
+        for p in cat["products"]:
+            prod.append(Product(**p))
+        cat["products"] = prod
+        category_list.append(Category(**cat))
+
+    return category_list
+
+if __name__ == "__main__":
+    prod_and_cat = read_json("../data/products.json")
+    category_data = creat_objects(prod_and_cat)
+    print(category_data)
+    print(category_data[0].name)
+    print(category_data[0].products)
