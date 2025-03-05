97d03692cf8e9e2ddca96fa22f0040955a99253b
Alex <magwai1@yandex.ru>
2025-03-05 21:30:54 +0400

    Создан абстрактный класс BaseProduct, реализован класс миксин

--- src/product_class.py
+++ src/product_class.py
@@ -1,4 +1,8 @@
-class Product:
+from src.base_product import BaseProduct
+from src.print_mixin import MixinLog
+
+
+class Product(MixinLog, BaseProduct):
     name = str
     description = str
     price = float
@@ -9,6 +13,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     @classmethod
     def new_product(cls, new_element):
